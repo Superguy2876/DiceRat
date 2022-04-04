7b89848f4a507c0a1fc24db3dcfc8489131f6343
dylan <patton_ist07@hotmail.com>
2022-04-04 22:51:11 +0900

    added new options to rolls, ad12, a will apply advantage, d will apply disadvantage, 1 will reroll 1s, and 2 will reroll 2s, also added new command, r20, will roll a single d20 with a single modifier, options also available.

--- commands/dice.py
+++ commands/dice.py
@@ -6,14 +6,24 @@ from lightbulb import commands
 # These two are the same type, but are optional. We can provide a
 # default value simply by using the `default` kwarg.
 @lightbulb.option("dice", "The dice to be rolled, will roll a single d20 if no value provided.", str, default='1d20')
-# @lightbulb.option("options", "The options to modify the type of dice roll. a for advantage, d for disadvantage, 1, to reroll 1s, and 12 to reroll 1s and 2s.", str, default='')
+@lightbulb.option("options", "The options to modify the type of dice roll. a for advantage, d for disadvantage, 1, to reroll 1s, and 12 to reroll 1s and 2s.", str, default='')
 @lightbulb.command("roll", "Roll one or more dice.")
 # Define the types of command that this function will implement
 @lightbulb.implements(commands.SlashCommand)
 async def dice(ctx: lightbulb.context.Context) -> None:
     # Extract the options from the context
     dice = ctx.options.dice
+    options = ctx.options.options
+
+    optionsList = ['a' in options, 'd' in options]
+    if '1' in options:
+        optionsList.append(1)
+    if '2' in options:
+        optionsList.append(2)
     
+    if optionsList[0] and optionsList[1]:
+        optionsList[0] = False
+        optionsList[1] = False
     diceList = []
     bonusList = []
 
@@ -23,7 +33,7 @@ async def dice(ctx: lightbulb.context.Context) -> None:
         await ctx.respond("Invalid dice format.")
         return
 
-    rolls, total = rollDice(diceList, bonusList)
+    rolls, total = rollDice(diceList, bonusList, optionsList)
 
     response = f" {' + '.join([str(roll[0])+':'+str(roll[1]) for roll in rolls])}"
     if len(bonusList) > 0:
@@ -38,12 +48,40 @@ async def dice(ctx: lightbulb.context.Context) -> None:
 
     await ctx.respond(response)
 
+@lightbulb.option("modifier", "The modifier to be added to the roll.", int, default=0)
+@lightbulb.option("options", "The options to modify the type of dice roll. a for advantage, d for disadvantage, 1 to reroll 1s", str, default='')
+@lightbulb.command("r20", "Roll a d20 with modifiers.")
+@lightbulb.implements(commands.SlashCommand)
+async def r20(ctx: lightbulb.context.Context) -> None:
+    modifier = ctx.options.modifier
+    options = ctx.options.options
+
+    optionsList = ['a' in options, 'd' in options]
+    if '1' in options:
+        optionsList.append(1)
+
+    rolls, total = rollDice([(1, 20)], [modifier], optionsList)
+
+    response = f" {' + '.join([str(roll[0])+':'+str(roll[1]) for roll in rolls])}"
+    if len(bonusList) > 0:
+        response += f" + {' + '.join([str(bonus) for bonus in bonusList])}"
+
+    response += f" = {total}"
+
+    if len(response) > 2000:
+        response = f"Dice body too long. Total roll: {total}."
+
+    response = f"Rolling 1d20\n{response}"
+
+    await ctx.respond(response)
+
 def load(bot: lightbulb.BotApp) -> None:
     bot.command(dice)
-
+    bot.command(r20)
 
 def unload(bot: lightbulb.BotApp) -> None:
     bot.remove_command(bot.get_slash_command("dice"))
+    bot.remove_command(bot.get_slash_command("r20"))
 
 def parseDice(dice):
     diceList = []
@@ -97,12 +135,28 @@ def parseDice(dice):
     
     return diceList, bonusList
 
-def rollDice(diceList, bonusList):
+def rollDice(diceList, bonusList, optionsList=[]):
     rolls = []
     for dice in diceList:
         rollGroup = []
         for _ in range(abs(dice[0])):
-            rollGroup.append( random.randint(1, dice[1]) * int(dice[0] / abs(dice[0])) )
+
+            roll1 = random.randint(1, dice[1]) * int(dice[0] / abs(dice[0]))
+
+            if optionsList[0] or optionsList[1]:
+                roll2 = random.randint(1, dice[1]) * int(dice[0] / abs(dice[0]))
+
+            if optionsList[0]:
+                droll = max(roll1, roll2)
+            elif optionsList[1]:
+                droll =min (roll1, roll2)
+            else:
+                droll = roll1 
+
+            if droll in optionsList:
+                droll = random.randint(1, dice[1]) * int(dice[0] / abs(dice[0]))
+
+            rollGroup.append(droll)
         rolls.append( ( f'{dice[0]}d{dice[1]}' , rollGroup) )
     
     total = sum([sum(roll[1]) for roll in rolls]) + sum(bonusList)
